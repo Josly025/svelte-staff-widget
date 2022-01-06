0f0fbad4203f7f81f1ac3c428b8e399939b92543
Grantjos12 <grantjoslyn@gmail.com>
2022-01-05 23:20:38 -0600

    inital files

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let name;
+
+  //IMPORT HERE
+  import Input from "./components/Input.svelte";
+</script>
+
+<main class="container">
+  <!--Components Here-->
+  <div class="uk-child-width-1-1@s uk-margin-large-top uk-grid-match" uk-grid>
+    <div>
+      <div
+        class="uk-card uk-card-primary uk-card-large uk-card-body uk-margin-left uk-margin-right"
+      >
+        <h3 class="uk-card-title uk-text-center uk-text-secondary">
+          Staffbase Svelte Widget
+        </h3>
+        <div class="uk-text-center">
+          <!--Components Insert Here-->
+          <Input />
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+
+<style>
+</style>
---
+++ src/components/Input.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<main>
+  <input
+    class="uk-input uk-form-width-medium uk-text-secondary uk-margin-medium-bottom"
+    type="text"
+    placeholder="Add Comment..."
+  />
+  <div class="uk-text-center" />
+  <button class="uk-button uk-button-secondary uk-button uk-width-1-4"
+    >Submit</button
+  >
+</main>
+
+<style>
+</style>
